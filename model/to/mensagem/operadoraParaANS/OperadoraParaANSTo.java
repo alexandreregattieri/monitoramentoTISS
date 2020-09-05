44bdc4458160706461aec4f4aa78e6f909d59435
Alexandre Prates Regattieri <36538143+alexandreregattieri@users.noreply.github.com>
2020-09-05 15:14:12 -0300

    Add files via upload

---
+++ model/to/mensagem/operadoraParaANS/OperadoraParaANSTo.java
@@ -0,0 +1,18 @@
+package model.to.mensagem.operadoraParaAns;
+
+import java.util.List;
+
+import model.to.mensagem.operadoraParaAns.guiaMonitoramento.GuiaMonitoramentoTo;
+
+public class OperadoraParaANSTo {
+
+	private List<GuiaMonitoramentoTo> guiaMonitoramento;
+
+	public List<GuiaMonitoramentoTo> getGuiaMonitoramento() {
+		return guiaMonitoramento;
+	}
+	public void setGuiaMonitoramento(List<GuiaMonitoramentoTo> guiaMonitoramento) {
+		this.guiaMonitoramento = guiaMonitoramento;
+	}
+	
+}
