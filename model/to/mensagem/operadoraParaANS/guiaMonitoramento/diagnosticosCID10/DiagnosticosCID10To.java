39c31c08a15c9e1e8a59301b6e1cba1a17c1ff57
Alexandre Prates Regattieri <36538143+alexandreregattieri@users.noreply.github.com>
2020-09-05 15:16:13 -0300

    Add files via upload

---
+++ model/to/mensagem/operadoraParaANS/guiaMonitoramento/diagnosticosCID10/DiagnosticosCID10To.java
@@ -0,0 +1,14 @@
+package model.to.mensagem.operadoraParaAns.guiaMonitoramento.diagnosticosCID10;
+
+public class DiagnosticosCID10To {
+
+	private String diagnosticoCID;
+
+	public String getDiagnosticoCID() {
+		return diagnosticoCID;
+	}
+	public void setDiagnosticoCID(String diagnosticoCID) {
+		this.diagnosticoCID = diagnosticoCID;
+	}
+	
+}
