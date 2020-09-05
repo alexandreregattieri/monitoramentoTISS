3ec3165b8fde3e5407937f826c1136fd77c20e67
Alexandre Prates Regattieri <36538143+alexandreregattieri@users.noreply.github.com>
2020-09-05 15:18:21 -0300

    Add files via upload

---
+++ model/to/mensagem/operadoraParaANS/guiaMonitoramento/valoresGuia/ValoresGuiaTo.java
@@ -0,0 +1,98 @@
+package model.to.mensagem.operadoraParaAns.guiaMonitoramento.valoresGuia;
+
+public class ValoresGuiaTo {
+	
+	private String valorTotalInformado;
+	private String valorProcessado;
+	private String valorTotalPagoProcedimentos;
+	private String valorTotalDiarias;
+	private String valorTotalTaxas;
+	private String valorTotalMateriais;
+	private String valorTotalOPME;
+	private String valorTotalMedicamentos;
+	private String valorGlosaGuia;
+	private String valorPagoGuia;
+	private String valorPagoFornecedores;
+	private String valorTotalTabelaPropria;
+	private String valorTotalCoParticipacao;
+	
+	public String getValorTotalInformado() {
+		return valorTotalInformado;
+	}
+	public void setValorTotalInformado(String valorTotalInformado) {
+		this.valorTotalInformado = valorTotalInformado;
+	}
+	public String getValorProcessado() {
+		return valorProcessado;
+	}
+	public void setValorProcessado(String valorProcessado) {
+		this.valorProcessado = valorProcessado;
+	}
+	public String getValorTotalPagoProcedimentos() {
+		return valorTotalPagoProcedimentos;
+	}
+	public void setValorTotalPagoProcedimentos(String valorTotalPagoProcedimentos) {
+		this.valorTotalPagoProcedimentos = valorTotalPagoProcedimentos;
+	}
+	public String getValorTotalDiarias() {
+		return valorTotalDiarias;
+	}
+	public void setValorTotalDiarias(String valorTotalDiarias) {
+		this.valorTotalDiarias = valorTotalDiarias;
+	}
+	public String getValorTotalTaxas() {
+		return valorTotalTaxas;
+	}
+	public void setValorTotalTaxas(String valorTotalTaxas) {
+		this.valorTotalTaxas = valorTotalTaxas;
+	}
+	public String getValorTotalMateriais() {
+		return valorTotalMateriais;
+	}
+	public void setValorTotalMateriais(String valorTotalMateriais) {
+		this.valorTotalMateriais = valorTotalMateriais;
+	}
+	public String getValorTotalOPME() {
+		return valorTotalOPME;
+	}
+	public void setValorTotalOPME(String valorTotalOPME) {
+		this.valorTotalOPME = valorTotalOPME;
+	}
+	public String getValorTotalMedicamentos() {
+		return valorTotalMedicamentos;
+	}
+	public void setValorTotalMedicamentos(String valorTotalMedicamentos) {
+		this.valorTotalMedicamentos = valorTotalMedicamentos;
+	}
+	public String getValorGlosaGuia() {
+		return valorGlosaGuia;
+	}
+	public void setValorGlosaGuia(String valorGlosaGuia) {
+		this.valorGlosaGuia = valorGlosaGuia;
+	}
+	public String getValorPagoGuia() {
+		return valorPagoGuia;
+	}
+	public void setValorPagoGuia(String valorPagoGuia) {
+		this.valorPagoGuia = valorPagoGuia;
+	}
+	public String getValorPagoFornecedores() {
+		return valorPagoFornecedores;
+	}
+	public void setValorPagoFornecedores(String valorPagoFornecedores) {
+		this.valorPagoFornecedores = valorPagoFornecedores;
+	}
+	public String getValorTotalTabelaPropria() {
+		return valorTotalTabelaPropria;
+	}
+	public void setValorTotalTabelaPropria(String valorTotalTabelaPropria) {
+		this.valorTotalTabelaPropria = valorTotalTabelaPropria;
+	}
+	public String getValorTotalCoParticipacao() {
+		return valorTotalCoParticipacao;
+	}
+	public void setValorTotalCoParticipacao(String valorTotalCoParticipacao) {
+		this.valorTotalCoParticipacao = valorTotalCoParticipacao;
+	}
+
+}
